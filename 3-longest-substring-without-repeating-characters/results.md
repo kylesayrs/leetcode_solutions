eaba19bf01bf81eff10be92b9080e213e70635af
Kyle Sayers <kylesayrs@gmail.com>
2023-11-05 21:20:07 -0500

    solution 3

---
+++ 3-longest-substring-without-repeating-characters/results.md
@@ -0,0 +1,7 @@
+Runtime
+4ms
+Beats 93.40%of users with C++
+
+Memory
+7.18MB
+Beats 97.13%of users with C++
--- 3-longest-substring-without-repeating-characters/solution.cpp
+++ 3-longest-substring-without-repeating-characters/solution.cpp
@@ -6,13 +6,32 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        return 0;
+        int start = 0;
+        int end = 0;
+
+        int longest_length = 0;
+
+        for (int end = 0; end < s.length(); end++) {
+            for (int index = start; index < end; index++) {
+                if (s[index] == s[end]) {
+                    start = index + 1;  // one after the offending position
+                    break;
+                }
+            }
+
+            int substring_length = end - start + 1;
+            if (substring_length > longest_length) {
+                longest_length = substring_length;
+            }
+        }
+
+        return longest_length;
     }
 };
 
 int main(int argc, char **argv) {
     Solution solution;
 
-    int length = solution.lengthOfLongestSubstring("");
+    int length = solution.lengthOfLongestSubstring("pwwkew");
     cout << length << endl;
 }
