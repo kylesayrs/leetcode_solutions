92199eb0d28cc8f7eff85a9940579563386cc06e
Kyle Sayers <kylesayrs@gmail.com>
2024-04-14 15:53:43 -0400

    521

---
+++ 521_longest-uncommon-subsequence-i/results.md
@@ -0,0 +1,7 @@
+Runtime
+34ms
+Beats 62.27% of users with Python3
+
+Memory
+16.48MB
+Beats 85.55% of users with Python3
---
+++ 521_longest-uncommon-subsequence-i/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findLUSlength(self, a: str, b: str) -> int:
+        # case len mismatch
+
+        # case character mismatch
+            # assert len(a) == len(b)
+            # return len(a)
+
+        # case order mismatch
+            # can use any of the strings, since including it will guarantee not match
+
+
+        if a == b:
+            return -1
+
+        return max(len(a), len(b))
