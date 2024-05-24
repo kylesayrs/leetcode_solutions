691ef9d871518b2e7cbf4d93df2c21f533876a8e
Kyle Sayers <kylesayrs@gmail.com>
2024-05-23 22:40:02 -0700

    67

---
+++ 67_add-binary/results.md
@@ -0,0 +1,7 @@
+Runtime
+4ms
+Beats 42.99% of users with C++
+
+Memory
+10.64MB
+Beats 20.42% of users with C++
--- 67_add-binary/solution.cpp
+++ 67_add-binary/solution.cpp
@@ -1,41 +1,39 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
-
 class Solution {
 public:
-    string addBinary(string a, string b) {
-        int a_index = 0;
-        int b_index = 0;
+    std::string addBinary(std::string a, std::string b) {
+        // TODO: check for edge cases
+        int a_index = a.size() - 1;
+        int b_index = b.size() - 1;
+        unsigned carry = 0;
 
-        cout << a[0] << endl;
-        cout << b[0] << endl;
+        std::string sum_string;
+        while (a_index >= 0 || b_index >= 0 || carry >= 1) {
+            unsigned a_part = a_index >= 0 ? static_cast<unsigned>(a[a_index]) - static_cast<unsigned>('0') : 0;
+            unsigned b_part = b_index >= 0 ? static_cast<unsigned>(b[b_index]) - static_cast<unsigned>('0') : 0;
 
-
-        string sum_string;
-        while (true) {
-            int a_digit = (a_index < a.size()) ? a[a_index] - '0' : 0;
-            int b_digit = (b_index < b.size()) ? b[b_index] - '0' : 0;
-            cout << a_digit << ", " << b_digit << endl;
-
-            if (a_digit == 0 && b_digit == 0) {
-                break;
+            unsigned part_sum = a_part + b_part + carry;
+            if (part_sum >= 2) {
+                part_sum -= 2;
+                carry = 1;
+            } else {
+                carry = 0;
             }
+    
+            sum_string = static_cast<char>(part_sum + '0') + sum_string;
 
-            sum_string = to_string(a_digit + b_digit) + sum_string;
-
-            a_index++;
-            b_index++;
+            a_index--;
+            b_index--;
         }
 
-        cout << sum_string << endl;
         return sum_string;
     }
 };
 
 
 int main() {
-    Solution().addBinary("1", "0");
+    Solution solution;
+    assert(solution.addBinary("01011", "1011") == "10110");
 }
